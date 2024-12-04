a4fcaa7e362e07656f798d245b7ced6be5e33bdc
Greg S <sourcetree@web.knobby.ws>
2024-12-04 08:33:51 -0800

    Going through in logical order
    added imageDescription
    
    removed focusBracketStepSize

--- PhotoName-Class.rb
+++ PhotoName-Class.rb
@@ -12,9 +12,9 @@ class Photo
 	
 	@@count = 0
 	
-	attr_accessor :id, :fn, :camModel, :fileType, :stackedImage, :driveMode, :afPointDetails, :focusBracketStepSize, :subjectTrackingMode, :createDate, :offsetTimeOriginal, :preservedFileName
+	attr_accessor :id, :fn, :camModel, :fileType, :stackedImage, :driveMode, :afPointDetails, :subjectTrackingMode, :createDate, :offsetTimeOriginal, :preservedFileName
 	# order by need for sorting and dealing with
-  def initialize(id, fn, camModel, fileType, stackedImage, driveMode, afPointDetails, focusBracketStepSize, subjectTrackingMode, createDate, offsetTimeOriginal, preservedFileName)
+  def initialize(id, fn, camModel, fileType, stackedImage, driveMode, afPointDetails, subjectTrackingMode, createDate, offsetTimeOriginal, preservedFileName)
 		
 # The counting needs some work, where did I get it from
 		# Every time a Photo (or a subclass of Photo) is instantiated,
@@ -33,15 +33,12 @@ class Photo
 		@stackedImage = stackedImage
 		@driveMode = driveMode
 		@afPointDetails = afPointDetails
-		@focusBracketStepSize = focusBracketStepSize
 		@subjectTrackingMode = subjectTrackingMode
 		@createDate = createDate
 		@offsetTimeOriginal = offsetTimeOriginal
 		@preservedFileName = preservedFileName
   end
 	
-	
-
 	
 	# if focusBracketStepSize.nil?
 	#   instructions = "STM: " + subjectTrackingModeOne + ". SM:" + shootingMode
@@ -62,12 +59,17 @@ end # class photo
 # srcHD = "testingClass/incomingTestPhotos"
 
 # Adding each file to Array photos
-src = "testingClass/incomingTestPhotos"
+# temp src until determine using. Full path needed to run from arbitrary place in iTerm. Relative works in Nova
+src = "/Users/gscar/Documents/Ruby/Photo handling/testingClass/incomingTestPhotos/" # Stacked w/ and w/o a stacked image, HHHR, THR, 
+# src = "/Volumes/Daguerre/_Download folder/_imported-archive/OM-1/OB[2024.11]-OM/" # 308 photos
+# src = "/Volumes/Daguerre/_Download folder/_imported-archive/OM-1/OA[2024.10]-OM/" # 476 photos
 # src = "testingClass/singleTestPhoto"
 id = 0
 photos = []
 lastPhotoFilename = "OB305994" # use later as starting point
-Dir.each_child(src) do |fn|
+# Dir.each_child(src) do |fn| # can be random order
+Dir.each_child(src).sort.each do |fn|
+	next if fn == '.DS_Store' # each_child knows about . and .. but not 
 	id += 1
   preservedFileName = fn
 	# redefine fn
@@ -81,14 +83,15 @@ Dir.each_child(src) do |fn|
 	# model = fileEXIF.model
 	driveMode = fileEXIF.DriveMode
 	subjectTrackingMode = fileEXIF.AISubjectTrackingMode
-	focusBracketStepSize = fileEXIF.FocusBracketStepSize
 	stackedImage = fileEXIF.StackedImage
 	afPointDetails = fileEXIF.AFPointDetails
 	filterEffect = fileEXIF.FilterEffect
-	focusBracketStepSize = fileEXIF.FocusBracketStepSize
+	# focusBracketStepSize = fileEXIF.FocusBracketStepSize # removed as didn't see any use
 	fileSubSecTimeOriginal = fileEXIF.SubSecTimeOriginal # no error if doesn't exist and it does not puts in OM
 	instructions = fileEXIF.instructions 
 	timeZoneOffset = fileEXIF.TimeZoneOffset
+	# parsed values
+	shotNo = driveMode.match(/(\d{1,3})/).to_s.rjust(2, '0')
 	# Feedback on what's being read. id, fn, camModel, fileType, stackedImage, driveMode, afPointDetails, focusBracketStepSize, subjectTrackingMode, createDate, offsetTimeOriginal, preservedFileName
 	# puts "Line no. #{__LINE__}
 	# id: #{id}
@@ -110,16 +113,42 @@ Dir.each_child(src) do |fn|
 	#   instructions: #{instructions} . should be blank for unprocessed photo
 	#   timeZoneOffset: #{timeZoneOffset} . should be blank for unprocessed photo
   # " 
+	
+	# 
+	imageDescription = "" # so not carried over from previous photo
+	# Only put in if value is present
+	if stackedImage != "No" # Always a value. Don't need to see No
+		imageDescription = stackedImage + " [StackedImage]. "
+	end
+	# puts "#{__LINE__}. driveMode: #{driveMode}.  driveMode.present?: #{driveMode.present?}."
+	if !driveMode.nil? # .present? didn't work
+		imageDescription << driveMode + " [DriveMode]. "
+	end
+	if !filterEffect.nil?
+		imageDescription << filterEffect + " [FilterEffect]. "
+	end
+	unless afPointDetails.nil?
+		imageDescription << afPointDetails + " [AFPointDetails]"
+	end
+	# unless focusBracketStepSize.nil?
+	# 	imageDescription << " ." + focusBracketStepSize.to_s + " [FocusBracketStepSize]"
+	# end
+	fileEXIF.description = imageDescription
+	# end imageDescription
+	fileEXIF.save # only change so far is imageDescription
+	
 	photo_id = "photo-" + id.to_s
-	photos <<	Photo.new(photo_id, fn, camModel, fileType, stackedImage, driveMode, afPointDetails, focusBracketStepSize, subjectTrackingMode, createDate, offsetTimeOriginal, preservedFileName)
+	photos <<	Photo.new(photo_id, fn, camModel, fileType, stackedImage, driveMode, afPointDetails, subjectTrackingMode, createDate, offsetTimeOriginal, preservedFileName)
 	# p photo_id
 	# puts "photo_id: #{photo_id}"
 	# puts "id: #{photo_id}. preservedFileName: #{photo_id.preservedFileName}"
-	
+	# Checking what is stored in stacked image
+	puts "#{id}. Stacked Image: `#{stackedImage}`. shotNo: #{shotNo}. driveMode: #{driveMode}.  preservedFileName: #{preservedFileName}"
 end
-# Accessing camModel for photo-10
+
+puts "\n#{__LINE__}.  Demo of retrieving info from array"
 photo_10 = photos.find { |photo| photo.id == "photo-10" }
-puts "#{__LINE__}. photo_id: photo-10.  Preserved File Name: #{photo_10.preservedFileName if photo_10}. createDate: #{photo_10.createDate}. "
+puts "#{__LINE__}. photo_id: photo-10.  Stacked Image: #{photo_10.stackedImage if photo_10}. photo_10.preservedFileName: #{photo_10.preservedFileName}. "
 # puts Photo.count
 # puts photo_id
 # Before made a list of files and copied. Will change with objects
